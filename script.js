7b5fffd2d4737a23318cd0d7964ea95d257dc714
L-lac <lena.lin.412@Gmail.com>
2024-12-21 20:21:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Handles user interaction with image track drag functionality 
+// invisible slider 
+const track = document.getElementbyId("image-track");
+window.onmousedown = e => {
+  track.dataset.mouseDownAt = e.clientX;
+}
