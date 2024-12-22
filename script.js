bae28a78c79ed5a490e4965c8d919fbc42d38692
L-lac <lena.lin.412@Gmail.com>
2024-12-21 21:12:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Handles user interaction with image track drag functionality 
 // invisible slider 
-const track = document.getElementbyId("image-track");
+const track = document.getElementById("image-track");
 
 window.onmousedown = e => {
   track.dataset.mouseDownAt = e.clientX;
@@ -8,14 +8,31 @@ window.onmousedown = e => {
 
 window.onmouseup = () => {
   track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage; 
 }
 
 window.onmousemove = e => {
   if(track.dataset.mouseDownAt === "0") return;
+  
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, 
     maxDelta = window.innerWidth / 2;
-  const percentage = (mouseDelta / maxDelta) * -100; 
-
-  track.style.transform = 'translate(${percentage}%, -50%)';
   
+  const percentage = (mouseDelta / maxDelta) * -100; 
+    let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+    //applying slider limits 
+    nextPercentage = Math.min(nextPercentage, 0);
+    nextPercentage = Math.max(nextPercentage, -100);
+
+    track.dataset.percentage = nextPercentage; 
+    track.animate({
+      transform: `translate(${nextPercentage}%, -50%)`
+    }, {duration: 1200, fill: "forwards"});
 }
+
+for(const image of track.getElementsByClassName("image")) {
+  image.animate({
+    objectPosition: `${100 + nextPercentage}% center`
+  }, {duration: 1200, fill: "forwards" });
+}
+
